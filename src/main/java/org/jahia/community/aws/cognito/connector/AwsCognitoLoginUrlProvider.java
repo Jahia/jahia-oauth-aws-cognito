6f7c40fc185bbd84f7a47e5a79df0030c6b1a79d
Thomas LE CLERE <tleclere@jahia.com>
2023-11-08 11:22:35 +0100

    Custom login parameters app & action

--- src/main/java/org/jahia/community/aws/cognito/connector/AwsCognitoLoginUrlProvider.java
+++ src/main/java/org/jahia/community/aws/cognito/connector/AwsCognitoLoginUrlProvider.java
@@ -98,7 +98,7 @@ public class AwsCognitoLoginUrlProvider implements LoginUrlProvider {
         }
         if (connectorConfig.getBooleanProperty(AwsCognitoConstants.WITH_CUSTOM_LOGIN)) {
             String loginUrl = connectorConfig.getProperty(AwsCognitoConstants.LOGIN_URL);
-            return StringUtils.isNotBlank(loginUrl) ? loginUrl + "&siteKey=" + siteKey : null;
+            return StringUtils.isNotBlank(loginUrl) ? loginUrl + "&app=jahia&action=login&siteKey=" + siteKey : null;
         }
         return jahiaOAuthService.getAuthorizationUrl(connectorConfig, sessionId, null);
     }
